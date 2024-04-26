5b8c29c49a5edd6403a704dde4b1942d64732e0e
Renan Dantas <renan@git.digitalrd.com.br>
2024-04-25 22:02:31 -0300

    docs: configuraçao inicial de projeto

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/presenter/app.config';
 import { AppComponent } from './app/presenter/pages/app/app.component';
-import { appConfig } from './app/presenter/pages/app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
